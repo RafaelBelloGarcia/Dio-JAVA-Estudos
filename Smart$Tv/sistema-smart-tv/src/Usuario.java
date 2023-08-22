efef12b3b0b48641799bd0bab1266a69b0271bdd
RafaelBelloGarcia <bello.garcia@gmail.com>
2023-08-22 10:24:39 -0300

    commits

---
+++ Smart$Tv/sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+        System.out.println("Tv ligada :" + smartTv.ligada);
+        System.out.println("canal atual :" + smartTv.canal);
+        System.out.println("volume atual :" + smartTv.volume);
+
+        smartTv.mudarCanal(13);
+        System.out.println("canal atual :" + smartTv.canal);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("volume atual :" + smartTv.volume);
+
+        smartTv.ligar();
+
+        System.out.println("Novo status: Tv ligada :" + smartTv.ligada);
+
+        smartTv.desligar();
+
+        System.out.println("Novo status: Tv ligada :" + smartTv.ligada);
+    }
+}
