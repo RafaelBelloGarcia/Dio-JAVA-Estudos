83d54e9d74e8da572df9d005dd44d5015aea9269
RafaelBelloGarcia <bello.garcia@gmail.com>
2023-08-23 13:46:27 -0300

    commit

---
+++ CaixaEletronico/ControleFluxo/src/ControleEscolar.java
@@ -0,0 +1,11 @@
+public class ControleEscolar {
+    public static void main(String[] args) throws Exception {
+        int nota = 6;
+
+        if (nota >= 7)
+            System.out.println("Aprovado");
+
+        else
+            System.out.println("Reprovado");
+    }
+}
