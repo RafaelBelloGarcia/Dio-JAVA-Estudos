c29a666b455e1e660380e07dded9273eebb052ef
RafaelBelloGarcia <bello.garcia@gmail.com>
2023-08-23 13:57:00 -0300

    commit

---
+++ CaixaEletronico/ControleFluxo/src/ControleEscolarEncadeada.java
@@ -0,0 +1,12 @@
+public class ControleEscolarEncadeada {
+    public static void main(String[] args) {
+        int nota = 7;
+
+        if (nota >= 7)
+            System.out.println("Aprovado");
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Recuperação");
+        else
+            System.out.println("Reprovado");
+    }
+}
