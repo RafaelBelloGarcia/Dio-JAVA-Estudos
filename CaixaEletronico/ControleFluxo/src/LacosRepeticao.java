79133ad33f0040f3484d34eb9b93f16d4e68638c
RafaelBelloGarcia <bello.garcia@gmail.com>
2023-08-24 09:42:04 -0300

    new commits

---
+++ CaixaEletronico/ControleFluxo/src/LacosRepeticao.java
@@ -0,0 +1,29 @@
+public class LacosRepeticao {
+    public static void main(String[] args) {
+	    String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+	    for (int x=0; x <alunos.length; x++) {
+		    System.out.println("O aluno no indice x= " + x + " é " + alunos[x]);
+	}
+
+}
+} 
+
+/*
+ *public class ExemploFor {
+ *  public static void main(String[] args) {
+ *	    String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+ *
+ *	    for (int x=0; x <alunos.length; x++) {
+ *		    System.out.println("O aluno no indice x= " + x + " é " + alunos[x]);
+ *  	}
+ *
+ *  }
+ *} 
+ */
+
+ /* ForEach:
+  * for(String aluno : alunos) {
+  *  System.out.println("O nome do Aluno é: " + aluno);
+  *}
+  */
